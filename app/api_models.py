7c2f679e16a46198906ffdb51ba3cdc62433ed88
Sergio Dolgov <sdolgov77@yandex.ru>
2024-10-14 14:22:17 +0600

    add all mehtods to api

--- app/api_models.py
+++ app/api_models.py
@@ -42,6 +42,17 @@ dept_parser.add_argument("phrase_case", type=int, choices=case_choices,
 dept_parser.add_argument("regime_init", type=str, required=False, choices=[
                          'init_as_is', 'init_lower'], help="Example - ['init_as_is', 'init_lower']", location="args")
 
+dept_init_parser = dept_parser.copy()
+dept_init_parser.remove_argument("phrase_case")
+
+dept_init_lower_parser = dept_init_parser.copy()
+dept_init_lower_parser.remove_argument("regime_init")
+
+init_parser = reqparse.RequestParser()
+init_parser.add_argument("word", type=str, required=False,
+                         help="Example - [Служба экологической безопасности]", location="args")
+
+
 post_parser = reqparse.RequestParser()
 post_parser.add_argument("post_name", type=str, required=False,
                          help="Example - [Ведущий специалист]", location="args")
@@ -77,3 +88,50 @@ count_parser.add_argument('p_cnt', type=int, required=True,
                           help="Example - [123]", location="args")
 count_parser.add_argument('sex', type=int, choices=[
                           0, 1, 2], required=True, help="Example - [0, 1]", location="args")
+
+
+count_pattern_parser = reqparse.RequestParser()
+count_pattern_parser.add_argument('p_cnt', type=int, required=True,
+                                  help="Example - [123]", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_one', type=str, required=True, help="Example - ['%s файл']", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_two', type=str, required=True, help="Example - ['%s файла']", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_five', type=str, required=True, help="Example - ['%s файлов']", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_null', type=str, required=False, help="Example - ['ошибка получения']", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_only_zero', type=str, required=False, help="Example - ['%s файлов']", location="args")
+count_pattern_parser.add_argument(
+    'pttrn_only_one', type=str, required=False, help="Example - ['%s файл']", location="args")
+count_pattern_parser.add_argument('sex', type=int, choices=[
+    0, 1, 2], required=False, help="Example - [0, 1, 2]", location="args")
+count_pattern_parser.add_argument("regime_init", type=str, required=False, choices=[
+    'init_as_is', 'init_lower', 'init_upper'], help="Example - ['init_as_is', 'init_lower', 'init_upper']", location="args")
+
+sum_parser = reqparse.RequestParser()
+sum_parser.add_argument('p_sum', type=int, required=True,
+                        help="Example - [123]", location="args")
+sum_parser.add_argument('currency_code', type=str, required=False,
+                        help="Example - [RUB]", location="args")
+sum_parser.add_argument('regime_init', type=str, required=False, choices=[
+                        'init_as_is', 'init_lower', 'init_upper'], help="Example - ['init_as_is', 'init_lower', 'init_upper']", location="args")
+
+upper_parser = reqparse.RequestParser()
+upper_parser.add_argument("text", type=str, required=False,
+                          help="Example - [Просто какой-то текст]", location="args")
+
+case_check_parser = reqparse.RequestParser()
+case_check_parser.add_argument('phrase_case', type=int,
+                               required=False, help="Example - [1]", location="args")
+
+regime_check_parser = reqparse.RequestParser()
+regime_check_parser.add_argument('regime', type=int,
+                                 required=False, help="Example - [1]", location="args")
+
+cut_post_parser = reqparse.RequestParser()
+cut_post_parser.add_argument('post_name', type=str, required=True,
+                             help="Example - [Ведущий специалист]", location="args")
+cut_post_parser.add_argument('dept_long_name', type=str, required=False,
+                             help="Example - [Служба экологической безопасности]", location="args")
--- app/resources.py
+++ app/resources.py
@@ -1,6 +1,8 @@
 from flask_restx import Resource, Namespace
 
-from .api_models import fio_parser, full_fio_parser, dept_parser, post_parser, date_parser, phrase_parser, count_parser
+from .api_models import fio_parser, full_fio_parser, dept_parser, post_parser, \
+    date_parser, phrase_parser, count_parser, dept_init_parser, dept_init_lower_parser, init_parser, \
+    count_pattern_parser, sum_parser, upper_parser, case_check_parser, regime_check_parser, cut_post_parser
 from .extensions import api, morphy, del_none
 
 
@@ -32,6 +34,42 @@ class DeptApi(Resource):
         args = dept_parser.parse_args()
 
         return morphy.dept(**args)
+
+
+@ns.route('/dept_init')
+class DeptInitApi(Resource):
+    @api.doc(parser=dept_init_parser)
+    def get(self):
+        args = dept_init_parser.parse_args()
+
+        return morphy.dept_init(**args)
+
+
+@ns.route('/dept_init_lower')
+class DeptInitLowerApi(Resource):
+    @api.doc(parser=dept_init_lower_parser)
+    def get(self):
+        args = dept_init_lower_parser.parse_args()
+
+        return morphy.dept_init_lower(**args)
+
+
+@ns.route('/init_lower')
+class InitLowerApi(Resource):
+    @api.doc(parser=init_parser)
+    def get(self):
+        args = init_parser.parse_args()
+
+        return morphy.init_lower(**args)
+
+
+@ns.route('/init_upper')
+class InitUpperApi(Resource):
+    @api.doc(parser=init_parser)
+    def get(self):
+        args = init_parser.parse_args()
+
+        return morphy.init_upper(**args)
 
 
 @ns.route('/post')
@@ -67,3 +105,60 @@ class PrintCountApi(Resource):
     def get(self):
         args = count_parser.parse_args()
         return morphy.print_count(**args)
+
+
+@ns.route('/print_count_pattern')
+class PrintCountPatternApi(Resource):
+    @api.doc(parser=count_pattern_parser)
+    def get(self):
+        args = count_pattern_parser.parse_args()
+        return morphy.print_count_pattern(**args)
+
+
+@ns.route('/print_sum')
+class PrintSumApi(Resource):
+    @api.doc(parser=sum_parser)
+    def get(self):
+        args = sum_parser.parse_args()
+        _args = del_none(args)
+        return morphy.print_sum(**_args)
+
+
+@ns.route('/upper')
+class UpperApi(Resource):
+    @api.doc(parser=upper_parser)
+    def get(self):
+        args = upper_parser.parse_args()
+        return morphy.upper(**args)
+
+
+@ns.route('/check_phrase_case')
+class CheckPhraseCaseApi(Resource):
+    @api.doc(parser=case_check_parser)
+    def get(self):
+        args = case_check_parser.parse_args()
+        return morphy.check_phrase_case(**args)
+
+
+@ns.route('/check_regime_length')
+class CheckRegimeLengthApi(Resource):
+    @api.doc(parser=regime_check_parser)
+    def get(self):
+        args = regime_check_parser.parse_args()
+        return morphy.check_regime_length(**args)
+
+
+@ns.route('/check_regime_post')
+class CheckRegimePostApi(Resource):
+    @api.doc(parser=regime_check_parser)
+    def get(self):
+        args = regime_check_parser.parse_args()
+        return morphy.check_regime_post(**args)
+
+
+@ns.route('/cut_post')
+class CutPostApi(Resource):
+    @api.doc(parser=cut_post_parser)
+    def get(self):
+        args = cut_post_parser.parse_args()
+        return morphy.cut_post(**args)
